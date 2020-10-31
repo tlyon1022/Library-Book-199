b003d295ec7e101b0c9c971280c1da501e6b161e
Tanner Lyon <54088092+tlyon1022@users.noreply.github.com>
2020-10-31 09:18:35 -0400

    Add files via upload

---
+++ Prog4/Prog4/LibraryBook.cs
@@ -0,0 +1,165 @@
+﻿// Program 4
+// CIS 199-XX
+// Due: 12/3/2019
+// By: Andrew L. Wright
+
+// File: LibraryBook.cs
+// This file creates a simple LibraryBook class capable of tracking
+// the book's title, author, publisher, copyright year, call number,
+// and checked out status.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class LibraryBook
+{
+    public const int DEFAULT_YEAR = 2019; // Default copyright year
+
+    private string _title;      // The book's title
+    private string _author;     // The book's author
+    private string _publisher;  // The book's publisher
+    private int _copyrightYear; // The book's year of copyright
+    private string _callNumber; // The book's call number in the library
+    private bool _checkedOut;   // The book's checked out status
+
+    // Precondition:  theCopyrightYear >= 0
+    // Postcondition: The library book has been initialized with the specified
+    //                values for title, author, publisher, copyright year, and
+    //                call number. The book is not checked out.
+    public LibraryBook(String theTitle, String theAuthor, String thePublisher,
+        int theCopyrightYear, String theCallNumber)
+    {
+        Title = theTitle;
+        Author = theAuthor;
+        Publisher = thePublisher;
+        CopyrightYear = theCopyrightYear;
+        CallNumber = theCallNumber;
+
+        ReturnToShelf(); // Make sure book is not checked out
+    }
+
+    public string Title
+    {
+        // Precondition:  None
+        // Postcondition: The title has been returned
+        get
+        {
+            return _title;
+        }
+
+        // Precondition:  None
+        // Postcondition: The title has been set to the specified value
+        set
+        {
+            _title = value;
+        }
+    }
+
+    public string Author
+    {
+        // Precondition:  None
+        // Postcondition: The author has been returned
+        get
+        {
+            return _author;
+        }
+
+        // Precondition:  None
+        // Postcondition: The author has been set to the specified value
+        set
+        {
+            _author = value;
+        }
+    }
+
+    public string Publisher
+    {
+        // Precondition:  None
+        // Postcondition: The publisher has been returned
+        get
+        {
+            return _publisher;
+        }
+
+        // Precondition:  None
+        // Postcondition: The publisher has been set to the specified value
+        set
+        {
+            _publisher = value;
+        }
+    }
+
+    public int CopyrightYear
+    {
+        // Precondition:  None
+        // Postcondition: The copyright year has been returned
+        get
+        {
+            return _copyrightYear;
+        }
+
+        // Precondition:  value >= 0
+        // Postcondition: The copyright year has been set to the specified value
+        set
+        {
+            if (value >= 0)
+                _copyrightYear = value;
+            else
+                _copyrightYear = DEFAULT_YEAR;
+        }
+    }
+
+    public string CallNumber
+    {
+        // Precondition:  None
+        // Postcondition: The call number has been returned
+        get
+        {
+            return _callNumber;
+        }
+
+        // Precondition:  None
+        // Postcondition: The call number has been set to the specified value
+        set
+        {
+            _callNumber = value;
+        }
+    }
+
+    // Precondition:  None
+    // Postcondition: The book is checked out
+    public void CheckOut()
+    {
+        _checkedOut = true;
+    }
+
+    // Precondition:  None
+    // Postcondition: The book is not checked out
+    public void ReturnToShelf()
+    {
+        _checkedOut = false;
+    }
+
+    // Precondition:  None
+    // Postcondition: true is returned if the book is checked out,
+    //                otherwise false is returned
+    public bool IsCheckedOut()
+    {
+        return _checkedOut;
+    }
+
+    // Precondition:  None
+    // Postcondition: A string is returned representing the libary book's
+    //                data on separate lines
+    public override string ToString()
+    {
+        string NL = Environment.NewLine; // Newline shortcut
+
+        return $"Title: {Title}{NL}Author: {Author}{NL}Publisher: {Publisher}{NL}" +
+            $"Copyright: {CopyrightYear}{NL}Call Number: {CallNumber}{NL}" +
+            $"Checked Out: {IsCheckedOut()}";
+    }
+}
---
+++ Prog4/Prog4/Program.cs
@@ -0,0 +1,72 @@
+﻿// Program 4
+// CIS 199-XX
+// Due: 12/3/2019
+// By: Andrew L. Wright
+
+// File: Program.cs
+// This file creates a simple test application class that creates
+// an array of LibraryBook objects and tests them.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+
+public class Program
+{
+    // Precondition:  None
+    // Postcondition: The LibraryBook class has been tested
+    public static void Main(string[] args)
+    {
+        LibraryBook book1 = new LibraryBook("The Wright Guide to C#", "Andrew Wright", "UofL Press",
+            2010, "ZZ25 3G");  // 1st test book
+        LibraryBook book2 = new LibraryBook("Harriet Pooter", "IP Thief", "Stealer Books",
+            2000, "AG773 ZF"); // 2nd test book
+        LibraryBook book3 = new LibraryBook("The Color Mauve", "Mary Smith", "Beautiful Books Ltd.",
+            1985, "JJ438 4T"); // 3rd test book
+        LibraryBook book4 = new LibraryBook("The Guan Guide to SQL", "Jeff Guan", "UofL Press",
+            2016, "ZZ24 4F");    // 4th test book
+        LibraryBook book5 = new LibraryBook("The Big Book of Doughnuts", "Homer Simpson", "Doh Books",
+            2001, "AE842 7A"); // 5th test book
+
+        LibraryBook[] theBooks = { book1, book2, book3, book4, book5 }; // Test array of books
+
+        WriteLine("Original list of books");
+        WriteLine("----------------------");
+        PrintBooks(theBooks);
+
+        // Make changes
+        book1.CheckOut();
+        book2.Publisher = "Borrowed Books";
+        book3.CheckOut();
+        book4.CallNumber = "AB123 4A";
+        book5.CheckOut();
+        book5.CopyrightYear = -1234; // Attempt invalid year
+
+        WriteLine("After changes");
+        WriteLine("-------------");
+        PrintBooks(theBooks);
+
+        // Return the books
+        book1.ReturnToShelf();
+        book3.ReturnToShelf();
+        book5.ReturnToShelf();
+
+        WriteLine("After returning the books");
+        WriteLine("-------------------------");
+        PrintBooks(theBooks);
+    }
+
+    // Precondition:  None
+    // Postcondition: The books have been printed to the console
+    public static void PrintBooks(LibraryBook[] books)
+    {
+        foreach (LibraryBook book in books)
+        {
+            WriteLine(book);
+            WriteLine();
+        }
+    }
+}
